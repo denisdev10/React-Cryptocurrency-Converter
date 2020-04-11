32dff25019f8b32843198448fd1c70b9782eb0ae
denisdev10 <strongweb102@gmail.com>
2020-04-12 00:04:52 +0400

    fix1

---
+++ build/precache-manifest.d673718d8cf4bf57326f4bb418e2b484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f8cf4dbf1b806d08e627eab1f8646f",
+    "url": "/cryptocurrency_converter_ReactJS_MobX_Material-UI/index.html"
+  },
+  {
+    "revision": "8493571720607556fa62",
+    "url": "/cryptocurrency_converter_ReactJS_MobX_Material-UI/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fde512d1fb0b61e1481d",
+    "url": "/cryptocurrency_converter_ReactJS_MobX_Material-UI/static/js/2.4782d76d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cryptocurrency_converter_ReactJS_MobX_Material-UI/static/js/2.4782d76d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8493571720607556fa62",
+    "url": "/cryptocurrency_converter_ReactJS_MobX_Material-UI/static/js/main.0943287a.chunk.js"
+  },
+  {
+    "revision": "edf45c3a4f1b52f22ba1",
+    "url": "/cryptocurrency_converter_ReactJS_MobX_Material-UI/static/js/runtime-main.b0dbcc1c.js"
+  }
+]);
