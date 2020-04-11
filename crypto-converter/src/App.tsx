b04a8dcfec83c0750efeee3f80367529f2d6d8f8
denisdev10 <strongweb102@gmail.com>
2020-04-11 16:54:24 +0400

    create left blok

--- crypto-converter/src/App.tsx
+++ crypto-converter/src/App.tsx
@@ -9,7 +9,12 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Typography from '@material-ui/core/Typography';
-
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -35,8 +40,27 @@ const useStyles = makeStyles((theme: Theme) =>
         currencyType: {
             minWidth: '30%',
         },
+        table: {
+            minWidth: 650,
+        },
     }),
 );
+
+
+
+
+
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 
 function App() {
@@ -48,7 +72,33 @@ function App() {
 
             <Grid container spacing={3}>
                 <Grid item xs={8}>
-                    <Paper className={classes.paper}>123123123</Paper>
+
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell></TableCell>
+                                    <TableCell align="left">Fullname</TableCell>
+                                    <TableCell align="left">Name</TableCell>
+                                    <TableCell align="left">Price</TableCell>
+                                    <TableCell align="left">volume24hour</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow key={row.name}>
+                                        <TableCell component="th" scope="row">
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell align="left">{row.calories}</TableCell>
+                                        <TableCell align="left">{row.fat}</TableCell>
+                                        <TableCell align="left">{row.carbs}</TableCell>
+                                        <TableCell align="left">{row.protein}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </Grid>
                 <Grid item xs={4}>
                     <Paper className={classes.paper}>
@@ -84,11 +134,9 @@ function App() {
                             </FormControl>
                         </div>
 
-
                         <Typography variant="h5" component="h5">
                             73,79 Российский рубль
                         </Typography>
-
 
                     </Paper>
                 </Grid>
