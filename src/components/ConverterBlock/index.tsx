cd72a4900d3e858587fe3563a8739a54913053f5
denisdev10 <strongweb102@gmail.com>
2020-04-12 18:28:54 +0400

    create  currency in selector

--- src/components/ConverterBlock/index.tsx
+++ src/components/ConverterBlock/index.tsx
@@ -6,59 +6,65 @@ import MenuItem from "@material-ui/core/MenuItem";
 import Paper from "@material-ui/core/Paper";
 import React from "react";
 import {TCoin} from "../../types";
+import {inject, observer} from "mobx-react";
+import CurrenciesStore from "../../stores/currenciesStore";
 
 
+type IConverterBlock = {
 
-
-type IConverterBlock ={
     classes: any;
+    currenciesStore?: CurrenciesStore;
 }
 
-const ConverterBlock: React.FC<IConverterBlock> = ({classes}) => {
 
-    return(
-
-
-        <Paper className={classes.paper}>
-            <div className={classes.cryptoInputBox}>
-                <FormControl className={classes.currencyInput}>
-                    <TextField fullWidth label="Сумма"/>
-                </FormControl>
-
-                <FormControl className={classes.currencyType}>
-
-                    <InputLabel id="demo-simple-select-helper-label">Валюта</InputLabel>
-                    <Select id="demo-simple-select" value={10}>
-                        <MenuItem value={10}>Ten</MenuItem>
-                        <MenuItem value={20}>Twenty</MenuItem>
-                        <MenuItem value={30}>Thirty</MenuItem>
-                    </Select>
-                </FormControl>
-            </div>
-
-            <div className={classes.cryptoInputBox}>
-                <FormControl className={classes.currencyInput}>
-                    <TextField fullWidth label="Сумма"/>
-                </FormControl>
-
-                <FormControl className={classes.currencyType}>
-
-                    <InputLabel id="demo-simple-select-helper-label">Валюта</InputLabel>
-                    <Select id="demo-simple-select" value={10}>
-                        <MenuItem value={10}>Ten</MenuItem>
-                        <MenuItem value={20}>Twenty</MenuItem>
-                        <MenuItem value={30}>Thirty</MenuItem>
-                    </Select>
-                </FormControl>
-            </div>
+const ConverterBlock: React.FC<IConverterBlock> = inject('currenciesStore')(
+    observer(({classes, currenciesStore}) => {
+        const coins: string[] = currenciesStore!.getItems.map(coin => coin.name);//отображение валют в селекторе
+        return (
 
 
-        </Paper>
+            <Paper className={classes.paper}>
+                <div className={classes.cryptoInputBox}>
+                    <FormControl className={classes.currencyInput}>
+                        <TextField fullWidth label="Сумма"/>
+                    </FormControl>
+
+                    <FormControl className={classes.currencyType}>
+
+                        <InputLabel id="demo-simple-select-helper-label">Валюта</InputLabel>
+
+                        <Select id="demo-simple-select" value={coins[0]}>
+                            {
+                                coins.map(name => <MenuItem value={name}>{name}</MenuItem>)
+                            }
+                        </Select>
+
+                    </FormControl>
+                </div>
+
+                <div className={classes.cryptoInputBox}>
+                    <FormControl className={classes.currencyInput}>
+                        <TextField fullWidth label="Сумма"/>
+                    </FormControl>
+
+                    <FormControl className={classes.currencyType}>
+
+                        <InputLabel id="demo-simple-select-helper-label">Валюта</InputLabel>
+
+                        <Select id="demo-simple-select" value={coins[0]}>
+                            {
+                                coins.map(name => <MenuItem value={name}>{name}</MenuItem>)
+                            }
+                        </Select>
+                    </FormControl>
+                </div>
 
 
+            </Paper>
 
-    )
 
-}
+        )
+
+    }))
 
 export default ConverterBlock;
